9259c58a966baf0e8a2ef4992014e7819db23ce8
devmarvis <erobagacollins@gmail.com>
2024-09-12 17:12:34 -0500

    load cafes from sql database and use jinja templating

--- main.py
+++ main.py
@@ -1,14 +1,46 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Float
-
-
-app = Flask(__name__)
+from sqlalchemy import Integer, String, Float, Boolean
 
 
 class Base(DeclarativeBase):
     pass
+
+
+db = SQLAlchemy(model_class=Base)
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///cafes.db"
+db.init_app(app)
+
+
+class Cafe(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    map_url: Mapped[int] = mapped_column(String(500), nullable=False)
+    img_url: Mapped[int] = mapped_column(String(500), nullable=False)
+    location: Mapped[str] = mapped_column(String(250), nullable=False)
+    seats: Mapped[str] = mapped_column(String(250), nullable=False)
+    has_toilet: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    has_sockets: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    has_wifi: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    can_take_calls: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
+
+    def to_dict(self):
+
+        dictionary = {}
+
+        for col in self.__table__.columns:
+            dictionary[col.name] = getattr(self, col.name)
+
+        return dictionary
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
@@ -18,7 +50,14 @@ def home():
 
 @app.route("/cafes")
 def cafes():
-    return render_template("cafes.html")
+    with app.app_context():
+        cafes = db.session.execute(db.select(Cafe)).scalars().all()
+    return render_template("cafes.html", cafes=cafes)
+
+
+@app.route("/add-cafe")
+def addCafe():
+    return render_template("add-cafe.html")
 
 
 if __name__ == '__main__':
