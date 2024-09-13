cc6fd3992ff8054fd674b4a7a731aa5055f09544
devmarvis <erobagacollins@gmail.com>
2024-09-13 10:24:34 -0500

    add new cafe form and remove cafe functionality

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Float, Boolean
@@ -19,8 +19,8 @@ db.init_app(app)
 class Cafe(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
-    map_url: Mapped[int] = mapped_column(String(500), nullable=False)
-    img_url: Mapped[int] = mapped_column(String(500), nullable=False)
+    map_url: Mapped[str] = mapped_column(String(500), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(500), nullable=False)
     location: Mapped[str] = mapped_column(String(250), nullable=False)
     seats: Mapped[str] = mapped_column(String(250), nullable=False)
     has_toilet: Mapped[bool] = mapped_column(Boolean, nullable=False)
@@ -51,13 +51,53 @@ def home():
 @app.route("/cafes")
 def cafes():
     with app.app_context():
-        cafes = db.session.execute(db.select(Cafe)).scalars().all()
-    return render_template("cafes.html", cafes=cafes)
+        cafes_ = db.session.execute(db.select(Cafe)).scalars().all()
+    return render_template("cafes.html", cafes=cafes_)
 
 
-@app.route("/add-cafe")
-def addCafe():
+@app.route("/add-cafe", methods=['POST', 'GET'])
+def add_cafe():
+    if request.method == 'POST':
+        name = request.form['cafe_name']
+        location = request.form['cafe_location']
+        url = request.form['cafe_url']
+        img = request.form['cafe_img']
+        price = request.form['coffee_price']
+        toilets = request.form['toilets_aval']
+        sockets = request.form['sockets_aval']
+        wifi = request.form['wifi_aval']
+        calls = request.form['calls_allow']
+        seats = request.form['seats_capacity']
+
+        with app.app_context():
+            db.create_all()
+            new_cafe = Cafe(
+                name=name,
+                map_url=url,
+                img_url=img,
+                location=location,
+                seats=seats,
+                has_toilet=bool(int(toilets)),
+                has_sockets=bool(int(sockets)),
+                has_wifi=bool(int(wifi)),
+                can_take_calls=bool(int(calls)),
+                coffee_price=price
+            )
+            db.session.add(new_cafe)
+            db.session.commit()
+            return redirect(url_for('cafes'))
     return render_template("add-cafe.html")
+
+
+@app.route("/remove/<cafe_name>")
+def remove(cafe_name):
+    print(cafe_name)
+    with app.app_context():
+        cafe_to_remove = db.session.execute(db.select(Cafe).where(Cafe.name == cafe_name)).scalars().first()
+        db.session.delete(cafe_to_remove)
+        db.session.commit()
+        print('Cafe successfully removed!')
+    return redirect(url_for('cafes'))
 
 
 if __name__ == '__main__':
